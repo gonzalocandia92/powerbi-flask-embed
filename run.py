29c6c0bdd4d4537ea7afbad4a848b00a652b677f
StivenAlexis <stivenmonsalvo22@gmail.com>
2026-05-28 18:52:22 -0300

    recovery run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 """
 Main entry point for the Power BI Flask Embed application.
 """
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2052, debug=False)
