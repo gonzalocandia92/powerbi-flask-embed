40bf35be9edde66eeedaf3195f12e89a75cfb02c
NBCHAD\c00candigi <gonzaloismael.cg@gmail.com>
2026-07-16 08:27:20 -0300

    Create 1512c11ce3b0_merge_whatsapp_and_analytics_heads.py

---
+++ migrations/versions/1512c11ce3b0_merge_whatsapp_and_analytics_heads.py
@@ -0,0 +1,24 @@
+"""Merge whatsapp and analytics heads
+
+Revision ID: 1512c11ce3b0
+Revises: b3d4e5f6a7c8, d2e3f4a5b6c7
+Create Date: 2026-07-16 08:25:28.211735
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1512c11ce3b0'
+down_revision = ('b3d4e5f6a7c8', 'd2e3f4a5b6c7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
